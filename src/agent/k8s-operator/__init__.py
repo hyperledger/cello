22966454d1ab3168f35db5c6b94acc42163ffd1c
Baohua Yang <yangbaohua@gmail.com>
2017-06-24 18:29:17 +0800

    [CE-68] Init the agent module for k8s-operator
    Change-Id: I628112dcca0ef2f5f4861f814205bdb2831e0b37
    Signed-off-by: Baohua Yang <yangbaohua@gmail.com>

---
+++ src/agent/k8s-operator/__init__.py
@@ -0,0 +1,6 @@
+# Copyright IBM Corp, All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#  This is for agent that interacts with Kubernetes platform using
+#  cello-k8s-operator, see github.com/hyperledger/cello-k8s-operator.
