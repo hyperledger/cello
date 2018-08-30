3e22eabbb3921299cc3bb93328968f7e449daedc
luke <jiahaochen1993@gmail.com>
2018-08-30 15:22:33 +0000

    [CE-441][CE-447] Fix SSL and remove redundant code
    Currently, error will be thrown while user try to creat
    kubernets type host with SSL enable and the get_config
    function will try to list all pods in kubernetes cluster,
    which is unnecessary since check_health function will do that,
    this patch is meant to fix the SSL bug and delete redundant code.
    
    CE-441 # done
    CE-447 # done
    
    Change-Id: I1ec1dccc59dace10f5c12cc64f6cec198eeff5ed
    Signed-off-by: luke <jiahaochen1993@gmail.com>

--- src/agent/k8s/host_operations.py
+++ src/agent/k8s/host_operations.py
@@ -63,20 +63,11 @@ class KubernetesOperation():
             k8s_config.verify_ssl = False
         else:
             k8s_config.verify_ssl = True
-            k8s_config.ssl_ca_cert = k8s_params.get('K8SSslCert')
+            k8s_config.ssl_ca_cert = \
+                config.kube_config. \
+                _create_temp_file_with_content(k8s_params.get('K8SSslCert'))
 
         client.Configuration.set_default(k8s_config)
-
-        v1 = client.CoreV1Api()
-        try:
-            v1.list_pod_for_all_namespaces(watch=False)
-        except Exception as e:
-            error_msg = (
-                "Cannot create kubernetes host due "
-                "to an incorrect parameters."
-            )
-            logger.error("Kubernetes host error msg: {}".format(e))
-            raise Exception(error_msg)
 
         return k8s_config
 
