fa1d790252ad014d0029551e2e7b401bb0f7ef8d
Haitao Yue <hightall@me.com>
2019-08-19 11:04:37 +0800

    [CE-679] Fix service deploy failed in prod MODE
    In prod MODE, not initial settings.py.
    
    Change-Id: I421b85c554754d61f3e5866272a11f25f7734c78
    Signed-off-by: Haitao Yue <hightall@me.com>

--- build_image/docker/common/api-engine/entrypoint.sh
+++ build_image/docker/common/api-engine/entrypoint.sh
@@ -2,10 +2,14 @@
 
 bash /scripts/initial.sh;
 holdup -t 120 tcp://${DB_HOST}:${DB_PORT};
-python manage.py makemigrations && python manage.py migrate;
-python manage.py create_user --username ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --is_superuser --email ${ADMIN_EMAIL} --role operator
-if [[ "$DEBUG" == "True" ]]; then
+if [[ "$RUN_MODE" == "server" ]]; then
+  python manage.py makemigrations && python manage.py migrate;
+  python manage.py create_user --username ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --is_superuser --email ${ADMIN_EMAIL} --role operator
+  if [[ "$DEBUG" == "True" ]]; then
     python manage.py runserver 0.0.0.0:8080;
-else
+  else
     uwsgi --ini /etc/uwsgi/apps-enabled/server.ini;
+  fi
+else
+  celery -A api_engine worker -l info
 fi
