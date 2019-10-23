43fea6fac71dde7118336921a1e97b92c7ead3e1
Haitao Yue <hightallyht@gmail.com>
2019-10-23 10:07:22 +0800

    [Fix #35] Remove unused images download operation
    Add api-engine/nginx/dashboard images download.
    
    Signed-off-by: Haitao Yue <hightallyht@gmail.com>

--- scripts/master_node/download_images.sh
+++ scripts/master_node/download_images.sh
@@ -34,7 +34,7 @@ echo_b "Check node:9.2 image."
 
 # docker image
 
-for IMG in baseimage engine mongo operator-dashboard user-dashboard watchdog parse-server ; do
+for IMG in dashboard nginx api-engine; do
 	HLC_IMG=hyperledger/cello-${IMG}
 	#if [ -z "$(docker images -q ${HLC_IMG}:${ARCH}-${VERSION} 2> /dev/null)" ]; then  # not exist
 	echo_b "Pulling ${HLC_IMG}:${ARCH}-${VERSION} from dockerhub"
