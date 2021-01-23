beef4a0654cb4b8254dc6a4a9523600625dd05ee
XuHugo <xq-310@163.com>
2021-01-23 09:16:18 +0800

    Modify organization
    Modify organization:
    *add config.py
    *modify organization
    
    Signed-off-by: XuHugo <xq-310@163.com>

---
+++ src/api-engine/api/config.py
@@ -0,0 +1,9 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+CELLO_HOME = "/opt/cello"
+FABRIC_TOOL = "/opt/bin"
+
+FABRIC_PEER_CFG = "/opt/node/peer.yaml.bak"
+FABRIC_ORDERER_CFG = "/opt/node/orderer.yaml.bak"
+FABRIC_CA_CFG = "/opt/node/ca.yaml.bak"
