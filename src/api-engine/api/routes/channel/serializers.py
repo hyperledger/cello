6cdd50fe4b2b865c8600e74d860c4bc5e4751e37
Yuanmao Zhu <zhu.yuanmao18@gmail.com>
2021-07-26 22:22:29 -0600

    addressed feedback and implemented channel list
    Signed-off-by: Yuanmao Zhu <zhu.yuanmao18@gmail.com>

--- src/api-engine/api/routes/channel/serializers.py
+++ src/api-engine/api/routes/channel/serializers.py
@@ -2,21 +2,31 @@
 from rest_framework import serializers
 
 from api.models import Channel
-
-
-class NodeSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=128)
-    hosts = serializers.ListField(
-        child=serializers.CharField(max_length=128)
-    )
+from api.common.serializers import ListResponseSerializer
 
 
 class ChannelCreateBody(serializers.Serializer):
     organization = serializers.UUIDField(help_text="ID of Organization")
     name = serializers.CharField(max_length=128, required=True)
-    peers = NodeSerializer(many=True)
-    orderers = NodeSerializer(many=True)
+    peers = serializers.ListField(
+        child=serializers.UUIDField(help_text="ID of Peer Nodes")
+    )
+    orderers = serializers.ListField(
+        child=serializers.UUIDField(help_text="ID of Orderer Nodes")
+    )
 
 
 class ChannelIDSerializer(serializers.Serializer):
     id = serializers.CharField(help_text="Channel ID")
+
+
+class ChannelResponseSerializer(ChannelIDSerializer, serializers.ModelSerializer):
+    network = serializers.UUIDField(help_text="ID of Network")
+
+    class Meta:
+        model = Channel
+        fields = ("name", "network", "create_ts")
+
+
+class ChannelListResponse(ListResponseSerializer):
+    data = ChannelResponseSerializer(many=True, help_text="Channel data")
--- src/api-engine/api/routes/channel/views.py
+++ src/api-engine/api/routes/channel/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from drf_yasg.utils import swagger_auto_schema
 from django.core.exceptions import ObjectDoesNotExist
+from api.common.serializers import PageQuerySerializer
+from django.core.paginator import Paginator
 
 from api.utils.common import with_common_response
 from api.auth import TokenAuth
@@ -13,13 +15,49 @@ from api.models import (
 )
 from api.routes.channel.serializers import (
     ChannelCreateBody,
-    ChannelIDSerializer
+    ChannelIDSerializer,
+    ChannelListResponse
 )
 
 
 class ChannelViewSet(viewsets.ViewSet):
     """Class represents Channel related operations."""
     authentication_classes = (JSONWebTokenAuthentication, TokenAuth)
+
+    @swagger_auto_schema(
+        query_serializer=PageQuerySerializer,
+        responses=with_common_response(
+            {status.HTTP_201_CREATED: ChannelListResponse}
+        ),
+    )
+    def list(self, request):
+        """
+        List Channels
+
+        :param request: org_id
+        :return: channel list
+        :rtype: list
+        """
+        serializer = PageQuerySerializer(data=request.GET)
+        if serializer.is_valid(raise_exception=True):
+            page = serializer.validated_data.get("page")
+            per_page = serializer.validated_data.get("per_page")
+
+            try:
+                org_id = request.user.organization
+                org = Organization.objects.get(pk=org_id)
+                channels = Channel.objects.filter(network=org.network)
+                p = Paginator(channels, per_page)
+                channels = p.page(page)
+                response = ChannelListResponse(
+                    data={"data": channels, "total": channels.count}
+                )
+                if response.is_valid(raise_exception=True):
+                    return Response(
+                        response.validated_data, status=status.HTTP_200_OK
+                    )
+            except ObjectDoesNotExist:
+                pass
 
     @swagger_auto_schema(
         request_body=ChannelCreateBody,
@@ -35,10 +73,10 @@ class ChannelViewSet(viewsets.ViewSet):
         :return: Channel ID
         :rtype: uuid
         """
+        org_id = request.user.organization
         serializer = ChannelCreateBody(data=request.data)
         if serializer.is_valid(raise_exception=True):
             name = serializer.validated_data.get("name")
-            org_id = serializer.validated_data.get("organization")
             peers = serializer.validated_data.get("peers")
             orderers = serializer.validated_data.get("orderers")
 
@@ -50,6 +88,7 @@ class ChannelViewSet(viewsets.ViewSet):
                 )
                 org.chanel = channel
                 org.save()
+                # TODO: Interact with the Fabric CLI.
                 response = ChannelIDSerializer(data=channel.__dict__)
                 if response.is_valid(raise_exception=True):
                     return Response(
