655fee03cc6619fad74b05c8db708b99568fcf74
fengyang_sy <fengyang.09186@h3c.com>
2022-03-14 15:20:08 +0800

    [#issue-423]Add upload chain code page Add upload chain code page
    Signed-off-by: fengyang_sy <fengyang.09186@h3c.com>

--- src/dashboard/src/locales/zh-CN/operatorChaincode.js
+++ src/dashboard/src/locales/zh-CN/operatorChaincode.js
@@ -7,4 +7,17 @@ export default {
   'app.operator.chainCode.table.header.md5': 'MD5',
   'app.operator.chainCode.table.operate.install': '安装',
   'app.operator.chainCode.table.operate.delete': '删除',
+  'app.operator.chainCode.form.create.header.title': '上传链码',
+  'app.operator.chainCode.form.create.name': '名称',
+  'app.operator.chainCode.form.create.checkName': '请输入链码名称',
+  'app.operator.chainCode.form.create.version': '版本',
+  'app.operator.chainCode.form.create.checkVersion': '请输入链码版本',
+  'app.operator.chainCode.form.create.language': '语言',
+  'app.operator.chainCode.form.create.checkLanguage': '请选择语言',
+  'app.operator.chainCode.form.create.fail': '上传链码失败',
+  'app.operator.chainCode.form.create.success': '上传链码成功',
+  'app.operator.chainCode.form.create.md5': 'md5',
+  'app.operator.chainCode.form.create.checkMd5': '请输入md5',
+  'app.operator.chainCode.form.create.file': '文件',
+  'app.operator.chainCode.form.create.fileSelect': '请选择链码文件'
 };
