87702d97452ecfec1887f41806d27e0358b49bf7
Yuanmao Zhu <yuanmao@ualberta.ca>
2022-04-03 16:54:55 +0000

    Update config variable.
    Signed-off-by: Yuanmao Zhu <yuanmao@ualberta.ca>

--- src/api-engine/api/config.py
+++ src/api-engine/api/config.py
@@ -4,8 +4,8 @@
 CELLO_HOME = "/opt/cello"
 FABRIC_TOOL = "/opt/bin"
 FABRIC_CFG = "/opt/node"
-FABRIC_NODE = "/opt/fabric"
-PRODUCTION_NODE = "/opt/production"
+FABRIC_NODE = "/opt/hyperledger/fabric"
+PRODUCTION_NODE = "/opt/hyperledger/production"
 
 FABRIC_PEER_CFG = "/opt/node/peer.yaml.bak"
 FABRIC_ORDERER_CFG = "/opt/node/orderer.yaml.bak"
