2b999dca98240e285cc14e3ea0d62eb7e45ea924
Yuanmao Zhu <yuanmao@ualberta.ca>
2023-08-20 00:21:48 -0400

    change variable names
    Signed-off-by: Yuanmao Zhu <yuanmao@ualberta.ca>

--- build_image/dockerhub/latest/common/api-engine/entrypoint.sh
+++ build_image/dockerhub/latest/common/api-engine/entrypoint.sh
@@ -12,9 +12,9 @@ holdup -t 120 tcp://${DB_HOST}:${DB_PORT};
 if [[ "$RUN_MODE" == "server" ]]; then
   python manage.py makemigrations && python manage.py migrate;
   python manage.py create_user \
-    --username ${ADMIN_USERNAME:-admin} \
-    --password ${ADMIN_PASSWORD:-pass} \
-    --email ${ADMIN_EMAIL:-admin@cello.com} \
+    --username ${API_ENGINE_ADMIN_USERNAME:-admin} \
+    --password ${API_ENGINE_ADMIN_PASSWORD:-pass} \
+    --email ${API_ENGINE_ADMIN_EMAIL:-admin@cello.com} \
     --is_superuser \
     --role admin
   if [[ "$DEBUG" == "True" ]]; then # For dev, use pure Django directly
